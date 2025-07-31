ee75a67b1db20c8aca3e2bcaad10781c51d490eb
minthantko <minthantko101@gmail.com>
2025-07-31 21:11:02 +0700

    done3

--- 331-intro-to-vite/src/App.vue
+++ 331-intro-to-vite/src/App.vue
@@ -8,7 +8,9 @@ const { message } = storeToRefs(store)
 </script>
 
 <template>
-  <div id="layout">
+  <div class="text-center font-sans text-gray-700 antialiased">
+    <!-- text-center: center the text of an element; 
+     anitaliased: to render text using grayscale antialiasing: -->
     <header>
       <div id="flashMessage" v-if="message">
         <h4>{{ message }}</h4>
@@ -27,14 +29,6 @@ const { message } = storeToRefs(store)
 </template>
 
 <style>
-  #layout {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
-
   nav {
     padding: 30px;
   }
